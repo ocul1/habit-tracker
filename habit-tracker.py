99a69056880758c810dc26f78d0d5f5021633b3c
ghidra <anclarke04@icloud.com>
2026-03-27 00:10:31 -0700

    added small stuff

---
+++ habit-tracker.py
@@ -0,0 +1,9 @@
+habit_tracker = True
+while habit_tracker == True:
+    print('Hello')
+    print('''
+        1. Reading
+        2. Exercise
+        3.
+          ''')
+    choice1 = input("What would you like to track today?: ")
